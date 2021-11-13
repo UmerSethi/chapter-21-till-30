51f62114a4914437e6042614769c1e7b1c269461
Umer Sethi <m.umersethi91@gmail.com>
2021-11-13 08:10:42 +0500

    // CHAPTER 21 - 30

---
+++ app.js
@@ -0,0 +1,45 @@
+        // CHAPTER 21 - 30
+
+// var firstName = prompt("Enter First Name")
+// var lastName = prompt("Enter Last Name")
+// var fullName = firstName + " " + lastName
+// alert("Hello! " + fullName)
+
+// var favMobile = prompt("Enter Your Favorite Mobile Name")
+// document.write("Your Favorite Phone is: " + favMobile)
+// document.write("<br> Length of String: " + favMobile.length)
+
+// var string1 = "Pakistani"
+// document.write("String: " + string1)
+// document.write("<br> Index of 'n': " + string1.indexOf("n"))
+
+// var string2 = "Hello World"
+// document.write("String: " + string2)
+// document.write("<br> Last Index of 'l': " + string2.lastIndexOf("l"))
+
+// var string3 = "Pakistani"
+// document.write("String: " + string3)
+// document.write("<br> Character at Index 3: " + string3.charAt(3))
+
+// var firstName = prompt("Enter First Name")
+// var lastName = prompt("Enter Last Name")
+// var fullName = firstName.concat(" "+lastName)
+// alert("Hello! " + fullName)
+
+
+// var city = "Hyderabad"
+// document.write("City: " + city)
+// city = city.replace("Hyder", "Islam")
+// document.write( city)
+
+// var numString = "472"
+// document.write("Value: " + numString)
+// document.write("<br> Type: " + typeof(numString))
+// numString = parseInt(numString)
+// document.write("<br> Value: " + numString)
+// document.write("<br> Type: " + typeof(numString))
+
+
+// var userInput = prompt("Enter Input")
+// document.write("User input: " + userInput)
+// document.write("<br> Upper case: " + userInput.toUpperCase())
